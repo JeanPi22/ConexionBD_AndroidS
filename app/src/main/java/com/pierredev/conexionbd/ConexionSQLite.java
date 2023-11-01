fc7f11e8e5390c5c36704f9c04f6dce0203c3ddf
Jean Pierre Peña <jean.photo.grafico@gmail.com>
2023-11-01 18:55:43 -0500

    Creación Boton (Crear base de datos) y creación de tabla de cliente, factura y pedido

--- app/src/main/java/com/pierredev/conexionbd/ConexionSQLite.java
+++ app/src/main/java/com/pierredev/conexionbd/ConexionSQLite.java
@@ -8,20 +8,53 @@ import androidx.annotation.Nullable;
 
 public class ConexionSQLite extends SQLiteOpenHelper {
 
-//    Constructor
-    public ConexionSQLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+//  Versión y nombre de la base de datos
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NOMBRE = "Ferreteria.db";
+
+//  Tablas de la base de datos
+    public static final String TABLE_CLIENTE = "t_cliente";
+    public static final String TABLE_FACTURA = "t_factura";
+    public static final String TABLE_PEDIDO = "t_pedido";
+    public static final String TABLE_PRODUCTO = "t_producto";
+    public static final String TABLE_PED_PROD = "t_ped_prod";
+
+//    Constructor Bd
+    public ConexionSQLite(@Nullable Context context) {
+        super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
     }
 
-//  Metodo para crear
+//  Metodo para crear Bd
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase FerreteriaDB) {
+
+        FerreteriaDB.execSQL("CREATE TABLE " + TABLE_CLIENTE + "(" +
+            "cod_cli INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "nombre TEXT," +
+            "direccion TEXT," +
+            "telefono TEXT)");
+
+        FerreteriaDB.execSQL("CREATE TABLE " + TABLE_FACTURA + "(" +
+                "cod_fact INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "fecha TEXT," +
+                "valor REAL," +
+                "cod_cli2 INTEGER," +
+                "FOREIGN KEY (cod_cli2) REFERENCES " + TABLE_CLIENTE + "(cod_cli))");
+
+        FerreteriaDB.execSQL("CREATE TABLE " + TABLE_PEDIDO + "(" +
+                "cod_ped INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "descripcion TEXT," +
+                "fecha TEXT," +
+                "cod_cli1 INTEGER," +
+                "cod_fact1 INTEGER," +
+                "FOREIGN KEY (cod_cli1) REFERENCES " + TABLE_CLIENTE + "(cod_cli)," +
+                "FOREIGN KEY (cod_fact1) REFERENCES " + TABLE_FACTURA + "(cod_fact))");
 
     }
 
-//  Metodo para actualizar
+//  Metodo para actualizar Bd
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase FerreteriaDB, int i, int i1) {
 
     }
 }
--- app/src/main/java/com/pierredev/conexionbd/MainActivity.java
+++ app/src/main/java/com/pierredev/conexionbd/MainActivity.java
@@ -3,12 +3,25 @@ package com.pierredev.conexionbd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCrear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//       Instanciamos Boton para crear base de datos
+
+        btnCrear = findViewById(R.id.btnCrear);
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
